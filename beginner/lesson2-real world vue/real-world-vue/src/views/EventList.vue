e552fece16f29ea3d13da26f54f4a85e614deaa8
Ovidiu Muresan <osilvium@gmail.com>
2020-04-17 01:08:50 +0200

    6. Single File Vue Components

--- beginner/lesson2-real world vue/real-world-vue/src/views/EventList.vue
+++ beginner/lesson2-real world vue/real-world-vue/src/views/EventList.vue
@@ -1,8 +1,15 @@
 <template>
-  <div>
-    <h1>Events Listing</h1>
-    <p>
-      <router-link :to="{ name: 'event-show', params: { id: '1' } }">First Event</router-link>
-    </p>
-  </div>
+	<div>
+		<h1>Events Listing</h1>
+		<EventCard/>
+	</div>
 </template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+export default {
+	components: {
+		EventCard
+	}
+}
+</script>
