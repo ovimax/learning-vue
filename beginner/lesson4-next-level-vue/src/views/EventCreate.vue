9a5ee7eba0c1656051e31421aa0bd39479fe79be
Ovidiu Muresan <osilvium@gmail.com>
2020-04-19 20:31:02 +0200

    7. Reusable Form Components: BaseInput

--- beginner/lesson4-next-level-vue/src/views/EventCreate.vue
+++ beginner/lesson4-next-level-vue/src/views/EventCreate.vue
@@ -2,27 +2,32 @@
   <div>
     <h1>Create an Event</h1>
     <form @submit.prevent="createEvent">
-      <label>Select a category</label>
-      <select v-model="event.category">
-        <option v-for="cat in categories" :key="cat">{{ cat }}</option>
-      </select>
+      <div class="field">
+        <label>Select a category</label>
+        <select v-model="event.categories">
+          <option v-for="cat in categories" :key="cat">{{ cat }}</option>
+        </select>
+      </div>
 
       <h3>Name & describe your event</h3>
-      <div class="field">
-        <label>Title</label>
-        <input v-model="event.title" type="text" placeholder="Add an event title"/>
-      </div>
+      <BaseInput label="Title" v-model="event.title" type="text" placeholder="Title" class="field"/>
 
-      <div class="field">
-        <label>Description</label>
-        <input v-model="event.description" type="text" placeholder="Add a description"/>
-      </div>
+      <BaseInput
+        label="Description"
+        v-model="event.description"
+        type="text"
+        placeholder="Description"
+        class="field"
+      />
 
       <h3>Where is your event?</h3>
-      <div class="field">
-        <label>Location</label>
-        <input v-model="event.location" type="text" placeholder="Add a location"/>
-      </div>
+      <BaseInput
+        label="Location"
+        v-model="event.location"
+        type="text"
+        placeholder="Location"
+        class="field"
+      />
 
       <h3>When is your event?</h3>
 
@@ -38,7 +43,7 @@
         </select>
       </div>
 
-      <input type="submit" class="button -fill-gradient" value="Submit"/>
+      <input type="submit" class="button -fill-gradient" value="Submit">
     </form>
   </div>
 </template>
@@ -99,9 +104,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-.field {
-  margin-bottom: 24px;
-}
-</style>
